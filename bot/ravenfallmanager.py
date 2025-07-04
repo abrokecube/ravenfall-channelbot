b4f13723cb148a849d3956b664ce81c776e97653
abrokecube <58220690+abrokecube@users.noreply.github.com>
2025-07-04 17:03:16 -0500

    gone

--- bot/ravenfallmanager.py
+++ bot/ravenfallmanager.py
@@ -70,7 +70,7 @@ class RFChannelManager:
         self.mult_check_routine.cancel()
         self.resync_routine.cancel()
         if self.rf_message_processor:
-            await self.rf_message_processor.stop()
+            self.rf_message_processor.stop()
 
     async def event_twitch_message(self, message: ChatMessage):
         for channel in self.channels:
