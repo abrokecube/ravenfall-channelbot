ccbe9d41d76626ff957776c9a15a19f46275d3fa
abrokecube <58220690+abrokecube@users.noreply.github.com>
2025-07-15 12:47:21 -0500

    let them have 30 seconds of multiplier

--- bot/ravenfallmanager.py
+++ bot/ravenfallmanager.py
@@ -163,15 +163,15 @@ class RFChannelManager:
             if channel.multiplier['multiplier'] != self.global_multiplier:
                 logger.debug(f"Multiplier mismatch for {channel.channel_name}: {channel.multiplier['multiplier']} != {self.global_multiplier}")
                 channel.queue_restart(30, label="multiplier mismatch", reason=RestartReason.MULTIPLIER_DESYNC)
-                # r = await send_multichat_command(
-                #     text=f"?say {channel.ravenbot_prefixes[0]}multiplier",
-                #     user_id=channel.channel_id,
-                #     user_name=channel.channel_name,
-                #     channel_id=channel.channel_id,
-                #     channel_name=channel.channel_name
-                # )
-                # if r['status'] != 200:
-                #     await channel.send_chat_message(f"?say {channel.ravenbot_prefixes[0]}multiplier")
+                r = await send_multichat_command(
+                    text=f"?say {channel.ravenbot_prefixes[0]}multiplier",
+                    user_id=channel.channel_id,
+                    user_name=channel.channel_name,
+                    channel_id=channel.channel_id,
+                    channel_name=channel.channel_name
+                )
+                if r['status'] != 200:
+                    await channel.send_chat_message(f"?say {channel.ravenbot_prefixes[0]}multiplier")
     
     @routine(delta=timedelta(seconds=120))
     async def resync_routine(self):
