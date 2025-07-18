e8eda64b817a02bad1cf0757864354e2f0398835
abrokecube <58220690+abrokecube@users.noreply.github.com>
2025-07-17 21:44:12 -0500

    ravenfall pid metric, hopefully works

--- bot/prom_metrics.py
+++ bot/prom_metrics.py
@@ -89,21 +89,59 @@ class Metrics:
             )
         return "\n".join(out_text)
 
-from typing import TYPE_CHECKING
+from typing import TYPE_CHECKING, List
 if TYPE_CHECKING:
     from .ravenfallmanager import RFChannelManager
+import psutil
+import os
+import asyncio
+from ..utils.runshell import runshell
 
 class MetricsManager:
     def __init__(self, rf_manager: 'RFChannelManager'):
         self.rf_manager = rf_manager
 
-    async def get_metrics(self) -> str:
-        m = Metrics()
+    async def desync_info(self, m: Metrics):
         desync_info = await self.rf_manager.get_desync_info()
         m.add_def("rf_ext_desync_seconds", "Estimated desync time in seconds", MetricType.GAUGE)
         for channel_name, desync in desync_info.items():
             m.add_value("rf_ext_desync_seconds", desync, channel=channel_name)
 
+    async def ravenfall_pids(self, m: Metrics):
+        ravenfall_pids = []
+        for proc in psutil.process_iter(['pid', 'name']):
+            try:
+                # Check if the process name matches (case-insensitive)
+                if "ravenfall" in proc.info['name'].lower():
+                    ravenfall_pids.append(proc.info['pid'])
+            except (psutil.NoSuchProcess, psutil.AccessDenied, psutil.ZombieProcess):
+                pass
+
+        tasks = []
+        for ch in self.rf_manager.channels:
+            shellcmd = (
+                f"\"{os.getenv('SANDBOXIE_START_PATH')}\" /box:{ch.sandboxie_box} /silent /listpids"
+            )
+        tasks.append(runshell(shellcmd))
+        responses: List[str | None] = await asyncio.gather(*tasks)
+        pid_lists = [x.splitlines() for code, x in responses]
+        box_pids = {}
+        for i in range(len(self.rf_manager.channels)):
+            box_pids[self.rf_manager.channels[i].channel_name] = pid_lists[i]
+
+        m.add_def("rf_ext_ravenfall_info", "Information about ravenfall", MetricType.GAUGE)
+        for ch in self.rf_manager.channels:
+            for pid in box_pids[ch.channel_name]:
+                if pid in ravenfall_pids:
+                    m.add_value("rf_ext_ravenfall_info", 1, channel=ch.channel_name, process_id=pid)
+
+    async def get_metrics(self) -> str:
+        m = Metrics()
+        tasks = [
+            self.desync_info(m),
+            self.ravenfall_pids(m)
+        ]
+        await asyncio.gather(*tasks, return_exceptions=True)
         return m.get_text()
 
         
