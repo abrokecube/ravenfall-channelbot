ce32305e07e58fdf60bf69edf877276d7c974f82
abrokecube <58220690+abrokecube@users.noreply.github.com>
2026-02-20 05:13:05 -0600

    oh

--- bot/commands/middlewares.py
+++ bot/commands/middlewares.py
@@ -3,14 +3,13 @@ from .global_context import GlobalContext
 
 TEXT_REPLACEMENTS = {
     "\U000e0000": None,
-    "\u034f": None
+    "\u034f": None,
 }
 TEXT_TRANS = str.maketrans(TEXT_REPLACEMENTS)
 def filter_text(text: str):
-    text = text.translate(TEXT_REPLACEMENTS)
+    text = text.translate(TEXT_TRANS)
     text = text.strip()
     return text
 
 def filter_message_event_text(global_ctx: GlobalContext, event: MessageEvent):
     event.text = filter_text(event.text)
-    
